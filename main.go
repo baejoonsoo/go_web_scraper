069c0864afdd1b08ad7c700e52b36a58eed09fb7
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-03-12 04:35:49 +0900

    Feat: goquery

--- main.go
+++ main.go
@@ -1,8 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
 
+	"github.com/PuerkitoBio/goquery"
+)
+
+var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main(){
-	fmt.Println("web Scraper")
+	getPage()
+}
+
+func getPage() int {
+	res, err := http.Get(baseURL)
+
+	checkErr(err)
+	checkStatusCode(res.StatusCode)
+
+	defer res.Body.Close()
+
+	doc,err := goquery.NewDocumentFromReader(res.Body)
+	
+	checkErr(err)
+
+	// fmt.Println(doc)
+	doc.Find(".pagination")
+
+	return 0	
+}
+
+func checkErr(err error){
+	if err != nil{
+		log.Fatalln(err)
+	}
+}
+
+func checkStatusCode(StatusCode int){
+	if StatusCode != 200{
+		log.Fatalln("Request failed with Status : ", StatusCode)
+	}
 }
