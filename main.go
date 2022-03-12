75a98a431ea1dd7a68a2101417ecdf38ec490d54
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-03-12 15:45:09 +0900

    Feat: get Page

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -10,10 +12,22 @@ import (
 var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main(){
-	getPage()
+	totalPages :=	getPages()
+	
+	for i := 0;i<totalPages; i++{
+		getPage(i)
+
+	}
 }
 
-func getPage() int {
+func getPage(page int){
+	pageURL := baseURL + "&start="+ strconv.Itoa(page*50)
+	fmt.Println("Requesting",pageURL)
+}
+
+
+func getPages() int {
+	pages := 0
 	res, err := http.Get(baseURL)
 
 	checkErr(err)
@@ -26,9 +40,11 @@ func getPage() int {
 	checkErr(err)
 
 	// fmt.Println(doc)
-	doc.Find(".pagination")
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length()
+	})
 
-	return 0	
+	return pages	
 }
 
 func checkErr(err error){
