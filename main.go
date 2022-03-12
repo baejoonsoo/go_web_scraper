07c7e5f4af49530d469775737a08265c96decc2d
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-03-12 16:40:25 +0900

    Feat: get jobs data

--- main.go
+++ main.go
@@ -9,7 +9,17 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	id string
+	location string
+	title string
+	salary string
+	summary string
+}
+
 var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
+
+var LastpageURL string = "https://kr.indeed.com/jobs?q=python&limit=50&start=9999"
 
 func main(){
 	totalPages :=	getPages()
@@ -23,28 +33,43 @@ func main(){
 func getPage(page int){
 	pageURL := baseURL + "&start="+ strconv.Itoa(page*50)
 	fmt.Println("Requesting",pageURL)
-}
 
-
-func getPages() int {
-	pages := 0
-	res, err := http.Get(baseURL)
+	res, err := http.Get(pageURL)
 
 	checkErr(err)
 	checkStatusCode(res.StatusCode)
 
 	defer res.Body.Close()
 
-	doc,err := goquery.NewDocumentFromReader(res.Body)
-	
+	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
 
-	// fmt.Println(doc)
-	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
-		pages = s.Find("a").Length()
-	})
+	searchCards := doc.Find(".tapItem")
 
-	return pages	
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		id,_ := card.Attr("data-jk")		
+		title:= card.Find(".jobTitle>span").Text()
+		location := card.Find(".companyLocation").Text()
+		summary := card.Find(".job-snippet").Text()
+		
+		fmt.Println(id, title, location, summary)
+	})
+}
+
+func getPages() int {
+	pages := 0
+	res, err := http.Get(LastpageURL)
+	checkErr(err)
+	checkStatusCode(res.StatusCode)
+	
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+	
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pages, err = strconv.Atoi(s.Find("b").Text())
+	})
+	return pages
 }
 
 func checkErr(err error){
@@ -52,9 +77,32 @@ func checkErr(err error){
 		log.Fatalln(err)
 	}
 }
-
-func checkStatusCode(StatusCode int){
-	if StatusCode != 200{
+	
+	func checkStatusCode(StatusCode int){
+		if StatusCode != 200{
 		log.Fatalln("Request failed with Status : ", StatusCode)
 	}
 }
+
+/*
+func getPages() int {
+pages := 0
+res, err := http.Get(baseURL)
+
+checkErr(err)
+checkStatusCode(res.StatusCode)
+
+defer res.Body.Close()
+
+doc,err := goquery.NewDocumentFromReader(res.Body)
+
+checkErr(err)
+
+// fmt.Println(doc)
+doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+	pages = s.Find("a").Length()
+})
+
+return pages	
+}
+*/
