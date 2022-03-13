76d2f7305d0cbf2a993d7ea5d74e3d631bfed30a
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-03-13 14:20:37 +0900

    Feat: Implementation of scraping csv file download function

--- main.go
+++ main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"os"
 	"strings"
 
 	cleanstring "github.com/baejoonsoo/webScraper/cleanString"
+	"github.com/baejoonsoo/webScraper/scraper"
 	"github.com/labstack/echo"
 )
+
+var fileName string = "jobs.csv"
 
 func handleHome(c echo.Context) error{
 	return c.File("home.html")
 }
 
 func handleScrape(c echo.Context) error{
+	defer os.Remove(fileName)
 	term := strings.ToLower(cleanstring.CleanString(c.FormValue("term")))
-	fmt.Println(term)
-	return nil
+	scraper.Scrape(term)
+	return c.Attachment(fileName,fileName)
 }
 
 func main(){
